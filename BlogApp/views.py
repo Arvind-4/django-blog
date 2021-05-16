3ec0be668bef75cccce4aa5a400df3ebc8e2ffd6
Arvind-4 <81419892+Arvind-4@users.noreply.github.com>
2021-05-16 11:41:21 +0530

    Add files via upload

---
+++ BlogApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+	return render(request, 'index.html',  context={})
+
+def about(request):
+	return render(request, 'about.html',  context={})
+
+def contact(request):
+	return render(request, 'contact.html',  context={})
+
+def post(request):
+	return render(request, 'post.html',  context={})
