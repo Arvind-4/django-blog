30dba0fb2521836c1018edd9a26aa69288df891d
Arvind-4 <lordarvindthegamer@gmail.com>
2023-12-08 19:15:13 +0530

    Removed: unused files

--- BlogApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, contact, about, post
-
-urlpatterns = [
-    path('', index, name='Index'),
-    path('post/', post, name='Post'),
-    path('about/', about, name='About'),
-    path('contact/', contact, name='Contact'),
-    ]
